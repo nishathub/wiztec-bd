b3b51037fc13b8e10722b70b62a74743f344eed4
Nishat-M <nishat.mohammad19@gmail.com>
2025-12-04 22:41:42 +0600

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+import "./homeStyle.css";
+
+const Home = () => {
+
+  return (
+   <div className="bg-black h-screen">Hello</div>
+  );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
